a4b6e65f155458bf1ab34d1da5c04b4ef2f6dcd5
komunal-dlieviant <daniel.lieviant@komunal.co.id>
2019-05-20 17:40:51 +0700

    added environment variable

--- src/models/user.js
+++ src/models/user.js
@@ -62,7 +62,7 @@ userSchema.virtual('tasks', {
 })
 
 userSchema.methods.generateAuthToken = async function() {
-    const token = jwt.sign({_id: this._id.toString()}, 'thisisasecret')
+    const token = jwt.sign({_id: this._id.toString()}, process.env.JWT_SECRET)
 
     this.tokens = this.tokens.concat({ token })
     await this.save()
